de556120dd1b0de821a8407b9580c12f9a3d26bb
Lelo nyaka <jeanrobertclass31@gmail.com>
2020-05-21 01:21:22 +0200

    implementation du service Catgorie service

--- src/main/java/com/ecom/mybio/services/CategoryMetierImpl.java
+++ src/main/java/com/ecom/mybio/services/CategoryMetierImpl.java
@@ -2,20 +2,27 @@ package com.ecom.mybio.services;
 
 import java.util.List;
 
-import com.ecom.mybio.entites.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ecom.mybio.entites.Category;
+import com.ecom.mybio.repository.CategoryRepository;
+
+@Service
 public class CategoryMetierImpl implements CategoryMetier {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public Category addCategory(Category cat) {
-		// TODO Auto-generated method stub
-		return null;
+		Category newCat = categoryRepository.save(cat);
+		return newCat;
 	}
 
 	@Override
 	public List<Category> listCategory() {
-		// TODO Auto-generated method stub
-		return null;
+		return categoryRepository.findAll();
 	}
 
 }
